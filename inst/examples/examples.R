ec7f7d6b83b777aa442d24f91887ed94469ca690
unknown <malbrech@L0502491.NMDP.ORG>
2015-03-02 15:02:04 -0600

    working example

--- inst/examples/examples.R
+++ inst/examples/examples.R
@@ -1,12 +1,7 @@
-library(dygraphs)
 library(dyPencilgraphs)
 
-hw <- HoltWinters(ldeaths)
-p <- predict(hw, n.ahead = 36, prediction.interval = TRUE, level = 0.95)
-ts <- cbind(ldeaths, p)
+ts <- ldeaths
 
 dyPencilgraph(ts, "Deaths from Lung Disease (UK)") %>%
-  dySeries("ldeaths", label = "Deaths") %>%
-  dySeries(c("p.lwr", "p.fit", "p.upr"), label = "Predicted") %>%
-  dyRangeSelector()
+  dySeries("V1", label = "Deaths")
 
