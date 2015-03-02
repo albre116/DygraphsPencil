592c56a9b623697cdd5135e2f9defeb8a7959c9c
unknown <malbrech@L0502491.NMDP.ORG>
2015-03-02 15:41:40 -0600

    working base example

--- inst/examples/examples.R
+++ inst/examples/examples.R
@@ -2,6 +2,6 @@ library(dyPencilgraphs)
 
 ts <- ldeaths
 
-dyPencilgraph(ts, "Deaths from Lung Disease (UK)") %>%
-  dySeries("V1", label = "Deaths")
-
+dyPencilgraph(ts, "Deaths from Lung Disease (UK)") %>%  ###you can only do 1 series or it will fail
+  dySeries("V1","Deaths") %>%
+  dyAxis("y",valueRange=c(min(ts),max(ts)))  ###you must specify the y values range or it will fail
--- inst/htmlwidgets/dyPencilgraphs.js
+++ inst/htmlwidgets/dyPencilgraphs.js
@@ -14,6 +14,14 @@ HTMLWidgets.widget({
   },
 
   renderValue: function(el, x, instance) {
+    //begin mark
+      var isDrawing = false;
+      var lastDrawRow = null; 
+      var lastDrawValue = null;
+      var tool = 'pencil';
+      var valueRange = x.attrs.axes.y.valueRange;
+      
+    //end mark
     
     // reference to this for closures
     var thiz = this;
@@ -77,8 +85,13 @@ HTMLWidgets.widget({
     } else {  // create new instance
       
       // add shiny input for date window
-      if (HTMLWidgets.shinyMode)
+      if (HTMLWidgets.shinyMode){
         this.addDateWindowShinyInput(el.id, x);
+        //begin mark
+        this.addDataCurveShinyInput(el.id, x);
+        this.addNrowNcolShinyInput(el.id, x);
+        //end mark
+      }
   
       // inject css if necessary
       if (x.css != null) {
@@ -90,6 +103,125 @@ HTMLWidgets.widget({
           style.appendChild(document.createTextNode(x.css));
         document.getElementsByTagName("head")[0].appendChild(style);
       }
+      
+      //begin mark
+      var setPoint = function(event, g, context) {
+        var canvasx = Dygraph.pageX(event) - Dygraph.findPosX(g.graphDiv);
+        var canvasy = Dygraph.pageY(event) - Dygraph.findPosY(g.graphDiv);
+        var xy = g.toDataCoords(canvasx, canvasy);
+        var x = xy[0], value = xy[1];
+        var rows = g.numRows();
+        var closest_row = -1;
+        var smallest_diff = -1;
+        // TODO(danvk): binary search
+        for (var row = 0; row < rows; row++) {
+          var date = g.getValue(row, 0);  // millis
+          var diff = Math.abs(date - x);
+          if (smallest_diff < 0 || diff < smallest_diff) {
+            smallest_diff = diff;
+            closest_row = row;
+          }
+        }
+
+        if (closest_row != -1) {
+          if (lastDrawRow === null) {
+            lastDrawRow = closest_row;
+            lastDrawValue = value;
+          }
+          var coeff = (value - lastDrawValue) / (closest_row - lastDrawRow);
+          if (closest_row == lastDrawRow) coeff = 0.0;
+          var minRow = Math.min(lastDrawRow, closest_row);
+          var maxRow = Math.max(lastDrawRow, closest_row);
+          for (var row = minRow; row <= maxRow; row++) {
+            if (tool == 'pencil') {
+              var val = lastDrawValue + coeff * (row - lastDrawRow);
+              val = Math.max(valueRange[0], Math.min(val, valueRange[1]));
+              attrs.file[row][1] = val;
+              if (val === null || value === undefined || isNaN(val)) {
+                console.log(val);
+              }
+            } else if (tool == 'eraser') {
+              attrs.file[row][1] = null;
+            }
+          }
+          lastDrawRow = closest_row;
+          lastDrawValue = value;
+          g.updateOptions({ file: attrs.file });
+          g.setSelection(closest_row);  // prevents the dot from being finnicky.
+        }
+      }
+      
+    var finishDraw = function() {
+        isDrawing = false;
+        lastDrawRow = null;
+        lastDrawValue = null;
+      }
+      //end mark
+      
+      //begin mark
+      
+      attrs.interactionModel = {
+              mousedown: function (event, g, context) {
+                if (tool == 'zoom') {
+                  Dygraph.defaultInteractionModel.mousedown(event, g, context);
+                } else {
+                  // prevents mouse drags from selecting page text.
+                  if (event.preventDefault) {
+                    event.preventDefault();  // Firefox, Chrome, etc.
+                  } else {
+                    event.returnValue = false;  // IE
+                    event.cancelBubble = true;
+                  }
+                  isDrawing = true;
+                  setPoint(event, g, context); 
+                }
+              },
+              mousemove: function (event, g, context) {
+                if (tool == 'zoom') {
+                  Dygraph.defaultInteractionModel.mousemove(event, g, context);
+                } else {
+                  if (!isDrawing) return;
+                  setPoint(event, g, context);
+                }
+              },
+              mouseup: function(event, g, context) {
+                if (tool == 'zoom') {
+                  Dygraph.defaultInteractionModel.mouseup(event, g, context);
+                } else {
+                  finishDraw();
+                }
+              },
+              mouseout: function(event, g, context) {
+                if (tool == 'zoom') {
+                  Dygraph.defaultInteractionModel.mouseout(event, g, context);
+                }
+              },
+              dblclick: function(event, g, context) {
+                Dygraph.defaultInteractionModel.dblclick(event, g, context);
+              },
+              mousewheel: function(event, g, context) {
+                var normal = event.detail ? event.detail * -1 : event.wheelDelta / 40;
+                var percentage = normal / 50;
+                var axis = g.xAxisRange();
+                var xOffset = g.toDomCoords(axis[0], null)[0];
+                var x = event.offsetX - xOffset;
+                var w = g.toDomCoords(axis[1], null)[0] - xOffset;
+                var xPct = w === 0 ? 0 : (x / w);
+
+                var delta = axis[1] - axis[0];
+                var increment = delta * percentage;
+                var foo = [increment * xPct, increment * (1 - xPct)];
+                var dateWindow = [ axis[0] + foo[0], axis[1] - foo[1] ];
+
+                g.updateOptions({
+                  dateWindow: dateWindow
+                });
+                Dygraph.cancelEvent(event);
+              }
+            };
+      
+      //end mark
+      
       
       // create the instance and add it to it's group (if any)
       instance.dygraph = new Dygraph(el, attrs.file, attrs);
@@ -438,6 +570,55 @@ HTMLWidgets.widget({
       Shiny.onInputChange(id + "_date_window", dateWindow); 
     };
   },
+      addNrowNcolShinyInput: function(id, x) {
+      
+    // check for an existing drawCallback
+    var prevDrawCallback = x.attrs["drawCallback"];
+    
+    // install the callback
+    x.attrs.drawCallback = function(me, initial) {
+      console.log("we are here at addNrowNcolShinyInput");
+      
+      // call existing
+      if (prevDrawCallback)
+        prevDrawCallback(me, initial);
+        
+      // fire input change
+      var ncol = me.numColumns();
+      var nrow = me.numRows();
+      var dimension = [nrow , ncol];
+      Shiny.onInputChange(id + "_data_dimension_RowCol", dimension); 
+    };
+  },
+  
+  
+    addDataCurveShinyInput: function(id, x) {
+      
+    // check for an existing drawCallback
+    var prevDrawCallback = x.attrs["drawCallback"];
+    
+    // install the callback
+    x.attrs.drawCallback = function(me, initial) {
+      
+      // call existing
+      if (prevDrawCallback)
+        prevDrawCallback(me, initial);
+        
+      // fire input change
+      
+      var ncol = me.numColumns();
+      var nrow = me.numRows();
+      var data = [];
+      for (var i = 0; i<nrow; i++){
+        for (var j = 0; j<ncol; j++){
+          data.push(me.getValue(i,j));
+        }
+      }
+      
+      Shiny.onInputChange(id + "_data_extract", data); 
+    };
+  },
+  
   
   // Add dashed line support to canvas rendering context
   // See: http://stackoverflow.com/questions/4576724/dotted-stroke-in-canvas
