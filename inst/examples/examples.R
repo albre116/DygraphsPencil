ee0bfe7e22588fe113bcf534b036b726671ed1fe
unknown <malbrech@L0502491.NMDP.ORG>
2015-03-04 09:19:13 -0600

    added libraries needed to work

--- inst/examples/examples.R
+++ inst/examples/examples.R
@@ -1,8 +1,12 @@
 library(dyPencilgraphs)
+library(datasets)
+library(xts)
+library(dplyr)
+library(dygraphs)
 
 ts <- ldeaths
 
 dyPencilgraph(ts, "Deaths from Lung Disease (UK)") %>%  ###you can only do 1 series or it will fail
-  dySeries("V1","Deaths") %>%
+  dySeries("V1","Deaths",fillGraph=T) %>%
   dyAxis("y",valueRange=c(min(ts),max(ts))) %>% ###you must specify the y values range or it will fail
   dyRangeSelector()
--- inst/examples/shiny/server.R
+++ inst/examples/shiny/server.R
@@ -10,7 +10,7 @@ shinyServer(function(input, output) {
   
   output$dyPencilgraph <- renderdyPencilgraph({
     dyPencilgraph(ts, "Deaths from Lung Disease (UK)") %>%  ###you can only do 1 series or it will fail
-      dySeries("V1","Deaths") %>%
+      dySeries("V1","Deaths",fillGraph=T) %>%
       dyAxis("y",valueRange=c(min(ts),max(ts))) %>% ###you must specify the y values range or it will fail
       dyRangeSelector()
     
